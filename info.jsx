60ae930b237bd1eed283283af91ffc6c7414c3ad
PrithaviBhanan786 <166564154+PrithaviBhanan786@users.noreply.github.com>
2024-04-16 00:54:26 +0530

    Add files via upload

---
+++ info.jsx
@@ -0,0 +1,7 @@
+const info = {
+    content:<>vChat is a submision for ExactSpace ReactJS assignment.<br/> Based on Assignment. <br/>Here are targets achieved: <br/>
+    <li>On clicking send button, show the chat message above the textbox.</li> 
+    <li>For each message, added a like button at the right end of the chat message, which on click should increase the count. Display the count next to the like button.</li>
+    <br/>Even <span style={{color:"Red"}}>Two Optional targets have been achieved </span>which are <li>mention list. Keypress '@' to see user list. You can select the user by clicking and it will complete the name in box.</li><li>Added emoji options in the chat message. Emoji button to see a popover window for emoji options. Click on emoji and it will be added into message input field.</li></>
+}
+export default info;
---
+++ src/App.jsx
@@ -0,0 +1,153 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import info from "../info";
+import EmojiPicker from "emoji-picker-react";
+import { Button, OverlayTrigger, Popover } from "react-bootstrap";
+
+
+function App() {
+  const [messages, setMsgs] = useState([
+    { message: "Knock Knock!", sentBy: "Alice", likes: 2 },
+    { message: "Who's there?", sentBy: "Bob", likes: 0 },
+    { message: "Says.", sentBy: "Alice", likes: 0 },
+    { message: "Says who?", sentBy: "Carol", likes: 0 },
+    { message: "Says me, that's who!", sentBy: "Alice", likes: 0 },
+  ]);
+  const [display, setDisplay] = useState("none");
+  const [user, setUser] = useState("");
+  const [msg, setMsg] = useState("");
+  const [users, setUsers] = useState([
+    "Alice",
+    "Bob",
+    "Pam",
+    "Carol",
+    "Dean",
+    "Elin",
+  ]);
+  let handleSubmit = () => {
+    if (msg != "" && user != "") {
+      console.log(user);
+      let dup = [...messages, { message: msg, sentBy: user, likes: 0 }];
+      // dup.push( { message: msg, sentBy: user, likes: 0 });
+      setMsgs(dup);
+      setMsg("");
+      setDisplay("none");
+      if (!users.includes(user)) {
+        dup = [...users];
+        dup.push(user);
+        setUsers(dup);
+      }
+    } else {
+      alert("don't send empty message/user");
+      setDisplay("none");
+    }
+  };
+  let handleChange = (e) => {
+    if (e.target.name == "user") {
+      setUser(e.target.value);
+    } else if (e.target.name == "message") {
+      setMsg(e.target.value);
+    }
+  };
+  let handleMentions = (e) => {
+    let dup = msg + e.target.value + " ";
+    setMsg(dup);
+    setDisplay("none");
+  };
+  let handleLike = (e) => {
+    let dup = [...messages];
+    // console.log(dup[e.target.name].likes)
+    dup[e.target.name].likes += 1;
+    setMsgs(dup);
+  };
+  let handleEmoji = (emoji,e) => {
+    let dup = msg + emoji.emoji;
+    setMsg(dup);
+  }
+  window.addEventListener("keypress", (e) => {
+    if (e.key == "@") {
+      setDisplay("flex");
+    }
+  });
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Body>
+        <div>
+          <EmojiPicker onEmojiClick={handleEmoji} />
+        </div>
+      </Popover.Body>
+    </Popover>
+  );
+  return (
+    <div className="App">
+      <div className="sidebar">
+        <h1>vChat</h1>
+        <div className="dir">Chat here &#8594;</div>
+        <div className="info">{info.content}</div>
+      </div>
+      <div className="chatbox">
+        <div className="view">
+          {messages.map((v, i) => (
+            <div className="msg-box" key={i}>
+              <div className="msg">
+                <span>{v.sentBy}: </span>
+                {v.message}
+              </div>
+              <div className="msg-box-input">
+                <span>&hearts;{v.likes}</span>
+                <input
+                  type="button"
+                  value="like"
+                  name={i}
+                  onClick={handleLike}
+                />
+              </div>
+            </div>
+          ))}
+          <div className="mentions" style={{ display: display }}>
+            {users.map((v, i) => (
+              <input
+                type="button"
+                value={v}
+                key={i}
+                name={v}
+                id={v}
+                onClick={handleMentions}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="input-region">
+          <OverlayTrigger trigger="click" placement="top" overlay={popover}>
+            <Button variant="success">🙂</Button>
+          </OverlayTrigger>
+          <input
+            type="text"
+            name="user"
+            id="user"
+            placeholder="Type username"
+            onChange={handleChange}
+            value={user}
+          />
+          <input
+            type="text"
+            name="message"
+            id="message"
+            placeholder="Type a message"
+            onChange={handleChange}
+            value={msg}
+          />
+          <input
+            type="button"
+            className="send"
+            value="Send"
+            onClick={handleSubmit}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
